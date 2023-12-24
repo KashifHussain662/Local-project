1213ee13bdc9c0f630cc2460306ff24cee01707f
KashifHussain662 <kashiif662@gmail.com>
2023-12-25 00:28:48 +0500

    cards section add

--- pages/script.js
+++ pages/script.js
@@ -1,9 +1,11 @@
 ScrollReveal({
   reset: true,
-  distance: "60px",
+  distance: "50px",
   duration: 2500,
   delay: 400,
 });
 ScrollReveal().reveal("#aside-pic", { delay: 500, origin: "left" });
 ScrollReveal().reveal("#aside-txt", { delay: 500, origin: "bottom" });
 ScrollReveal().reveal("#aside-btn", { delay: 500, origin: "right" });
+ScrollReveal().reveal("#card", { delay: 500, origin: "left" });
+ScrollReveal().reveal("#card-ttl", { delay: 500, origin: "top" });
