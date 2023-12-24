dc58f281fe02aa3ffd3a6d7673c0626aa17710d7
KashifHussain662 <kashiif662@gmail.com>
2023-12-24 21:31:27 +0500

    sectiont-two add

---
+++ pages/script.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+ScrollReveal().reveal("#aside-pic", { delay: 500, origin: "left" });
+ScrollReveal().reveal("#aside-txt", { delay: 500, origin: "bottom" });
+ScrollReveal().reveal("#aside-btn", { delay: 500, origin: "right" });
